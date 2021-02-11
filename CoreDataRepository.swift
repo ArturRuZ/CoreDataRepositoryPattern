afa9e9406b5518cbf32aff33a2dd714a7c838527
Artur <43502269+ArturRuZ@users.noreply.github.com>
2021-02-11 12:15:00 +0300

    Update CoreDataRepository.swift
    implementation completed . Testing started

--- CoreDataRepository.swift
+++ CoreDataRepository.swift
@@ -6,47 +6,106 @@
 //
 
 
-// TEMPLATE OF USAGE
-/*
-  class TestRepository: Repository<DBRepoRawData, DBRepoObjectModel> {
 
-  override var actualData: Observable<[DBRepoObjectModel]>? {
-     return nil
-  }
-  
-  override func save(_ objects: [DBRepoObjectModel], completion: @escaping ((Result<Void>) -> Void)) {
-  }
-  override func save(_ rawData: [DBRepoRawData], completion: @escaping ((Result<Void>) -> Void)) {
-  }
-  override func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[DBRepoObjectModel]>) -> Void))
-  {
-  }
-  override func delete(by search: RepositorySearchRequest, completion: @escaping ((Result<Void>) -> Void)) {
-  }
-  }
+// TEMPLATE OF REPOSITORY USAGE
+/*
+ class TestRepository: Repository<DBRepoRawData, DBRepoExpectedModel> {
+
+ override var actualSearchedData: Observable<[DBRepoExpectedModel]>? {
+    return nil
+ }
+ 
+ override func save(_ objects: [DBRepoExpectedModel], completion: @escaping ((Result<Void>) -> Void)) {
+ }
+ override func save(_ rawData: [DBRepoRawData], completion: @escaping ((Result<Void>) -> Void)) {
+ }
+ override func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[DBRepoExpectedModel]>) -> Void))
+ {
+ }
+ override func delete(by search: RepositorySearchRequest, completion: @escaping ((Result<Void>) -> Void)) {
+ }
+ }
+ */
+// Convertable CoreData USAGE
+/*
+ import Foundation
+ import CoreData
+
+ public protocol MockRawData {
+    
+ }
+ public protocol MockModel {
+    
+ }
+ 
+ @objc(Problems8dEntity)
+ public class TestEntity: EntityConvertable<MockRawData, MockModel>  {
+
+    public override func updateSelfFrom(_ model: MockModel) {
+       
+    }
+    public override func updateSelfFrom(_ rawData:  MockRawData) {
+       
+    }
+    public override func transformSelfToModel() -> MockModel? {
+       return nil
+    }
+ }
  */
 
 
 import Foundation
 import CoreData
 
+//MARK: - Common repository protocol + default implementation
 protocol AccessableRepository {
-   associatedtype RawData: Codable
-   associatedtype ObjectModel
+   associatedtype RawDataForUpdate
+   associatedtype ExpectedModel
    
-   var actualData: Observable<[ObjectModel]>? {get}
+   var actualSearchedData: Observable<[ExpectedModel]>? {get}
    
-   func save(_ rawData: [RawData], completion: @escaping ((Result<Void>) -> Void))
-   func save(_ objects: [ObjectModel], completion: @escaping ((Result<Void>) -> Void))
+   func save(_ rawData: [RawDataForUpdate], completion: @escaping ((Result<Void>) -> Void))
+   func save(_ objects: [ExpectedModel], completion: @escaping ((Result<Void>) -> Void))
    func delete(by search: RepositorySearchRequest, completion: @escaping ((Result<Void>) -> Void))
-   func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[ObjectModel]>) -> Void))
+   func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[ExpectedModel]>) -> Void))
 }
 
-protocol EntityConvertable {
-   func updateSelfFrom(schema: Codable)
-   func transformSelfTo<T>(expectedType: T.Type) -> T?
+//MARK: - The way of fixing protocol error for generic types for AccessableRepository
+class Repository<RepoRawData, RepoDomainExpectedModel>: NSObject, AccessableRepository {
+   typealias RawDataForUpdate = RepoRawData
+   typealias ExpectedModel = RepoDomainExpectedModel
+   
+   var actualSearchedData: Observable<[RepoDomainExpectedModel]>? { nil }
+   
+   func save(_ rawData: [RepoRawData], completion: @escaping ((Result<Void>) -> Void)) {}
+   func save(_ objects: [RepoDomainExpectedModel], completion: @escaping ((Result<Void>) -> Void)) {}
+   func delete(by search: RepositorySearchRequest, completion: @escaping ((Result<Void>) -> Void)) {}
+   func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[RepoDomainExpectedModel]>) -> Void)) {}
 }
 
+//MARK:- CoreData repository
+
+//MARK:- EntityConvertation protocol + implementation
+protocol EntityConvertation where Self: NSManagedObject   {
+   associatedtype RawDataForUpdate
+   associatedtype ExpectedModel
+   
+   func updateSelfFrom(_ rawData: RawDataForUpdate)
+   func updateSelfFrom(_ model: ExpectedModel)
+   func transformSelfToModel() -> ExpectedModel?
+}
+
+//MARK: The way of fixing protocol error for generic types for EntityConvertation
+public class EntityConvertable<RawData, DomainModel>: NSManagedObject, EntityConvertation {
+   public typealias RawDataForUpdate = RawData
+   public typealias ExpectedModel = DomainModel
+   
+   public func updateSelfFrom(_ rawData: RawData) {}
+   public func updateSelfFrom(_ model: DomainModel) {}
+   public func transformSelfToModel() -> DomainModel? { return nil }
+}
+
+//MARK:- Helpers for CoreData Rrpository default implementation
 protocol DBContextProviding {
    func mainQueueContext() -> NSManagedObjectContext
    func performBackgroundTask(_ block: @escaping (NSManagedObjectContext) -> Void)
@@ -59,31 +118,21 @@ protocol RepositorySearchRequest {
    var sortDescriptors: [NSSortDescriptor] {get}
 }
 
-enum RepositoryErrors: Error {
+enum DBRepositoryErrors: Error {
    case entityConvertableMissing
+   case noChangesInBase
 }
-//extension RepositoryErrors: LocalizedError {
+//extension DBRepositoryErrors: LocalizedError {
 //
 //}
 
-//MARK: The way of fixing protocol error for generic types
-class Repository<RepoRawData: Codable, RepoObjectModel>: AccessableRepository {
-   typealias RawData = RepoRawData
-   typealias ObjectModel = RepoObjectModel
+//MARK: - CoreData repository default implementation
+final class DBRepository<DBRepoRawData, DBRepoExpectedModel, DBRepoGettingFilter>: Repository<DBRepoRawData, DBRepoExpectedModel>, NSFetchedResultsControllerDelegate  {
    
-   var actualData: Observable<[RepoObjectModel]>? { nil }
-   
-   func save(_ rawData: [RepoRawData], completion: @escaping ((Result<Void>) -> Void)) {}
-   func save(_ objects: [RepoObjectModel], completion: @escaping ((Result<Void>) -> Void)) {}
-   func delete(by search: RepositorySearchRequest, completion: @escaping ((Result<Void>) -> Void)) {}
-   func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[RepoObjectModel]>) -> Void)) {}
-}
-
-//MARK: Repository implementation for CoreData usage
-final class DBRepository<DBRepoRawData: Codable, DBRepoObjectModel, DBRepoGettingFilter>: Repository<DBRepoRawData, DBRepoObjectModel> {
-   
-   private let associatedEntity: String
+   private let associatedEntityName: String
    private let contextSource: DBContextProviding
+   private var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>?
+   private var searchedData: Observable<[DBRepoExpectedModel]>?
    
    private func saveIn(context: NSManagedObjectContext, mergePolicy: Any = NSMergeByPropertyObjectTrumpMergePolicy, completion: ((Result<Void>) -> Void)? = nil) {
       context.mergePolicy = mergePolicy
@@ -99,66 +148,89 @@ final class DBRepository<DBRepoRawData: Codable, DBRepoObjectModel, DBRepoGettin
          completion?(Result(value: ()))
       case false:
          ConsoleLog.logEvent(object: "DBRepository", method: "saveIn", "No changes in context")
-         completion?(Result(error: ErrorsList.noChangesInBase))
+         completion?(Result(error: DBRepositoryErrors.noChangesInBase))
       }
-      
    }
    
-   private func configureActualDataUpdating(_ request: RepositorySearchRequest) {
+   private func configureactualSearchedDataUpdating(_ request: RepositorySearchRequest) -> NSFetchedResultsController<NSFetchRequestResult> {
+      let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: associatedEntityName)
+
+      fetchRequest.predicate = request.predicate
+      fetchRequest.sortDescriptors = request.sortDescriptors
       
+      let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                                managedObjectContext: contextSource.mainQueueContext(), sectionNameKeyPath: nil,
+                                                                    cacheName: nil)
+      fetchedResultsController.delegate = self
+      
+      return fetchedResultsController
+
    }
    
-   init(associatedEntity: String, contextSource: DBContextProviding, autoUpdateSearchRequest: RepositorySearchRequest?) {
-      self.associatedEntity = associatedEntity
+   init(associatedEntityName: String, contextSource: DBContextProviding, autoUpdateSearchRequest: RepositorySearchRequest?) {
+      self.associatedEntityName = associatedEntityName
       self.contextSource = contextSource
       super.init()
       
-      guard let request = autoUpdateSearchRequest else { return }
-      configureActualDataUpdating(request)
+      guard let request = autoUpdateSearchRequest else { return  }
+      self.fetchedResultsController = configureactualSearchedDataUpdating(request)
+      self.searchedData  = .init(value: [])
    }
    
    //MARK: - Overriding default Repository implementation
-   override var actualData: Observable<[DBRepoObjectModel]>? {
-      return nil
+   override var actualSearchedData: Observable<[DBRepoExpectedModel]>? {
+      return searchedData
    }
    
-   override func save(_ objects: [DBRepoObjectModel], completion: @escaping ((Result<Void>) -> Void)) {
-      
-   }
-   override func save(_ rawData: [DBRepoRawData], completion: @escaping ((Result<Void>) -> Void)) {
+   override func save(_ objects: [DBRepoExpectedModel], completion: @escaping ((Result<Void>) -> Void)) {
       contextSource.performBackgroundTask() { context in
          // TO DO - Check case when CoreData not update someValues
-         guard let _ = NSEntityDescription.insertNewObject(forEntityName: self.associatedEntity, into: context) as? EntityConvertable else { return completion(Result(error: RepositoryErrors.entityConvertableMissing)) }
+         guard let _ = NSEntityDescription.insertNewObject(forEntityName: self.associatedEntityName, into: context) as? EntityConvertable<DBRepoRawData, DBRepoExpectedModel> else { completion(Result(error: DBRepositoryErrors.entityConvertableMissing))
+            return assert(false, DBRepositoryErrors.entityConvertableMissing.localizedDescription)
+         }
          
-         rawData.forEach({
-            let entity = NSEntityDescription.insertNewObject(forEntityName: self.associatedEntity, into: context) as? EntityConvertable
-            entity?.updateSelfFrom(schema: $0)
+         objects.forEach({
+            let entity = NSEntityDescription.insertNewObject(forEntityName: self.associatedEntityName, into: context) as? EntityConvertable<DBRepoRawData, DBRepoExpectedModel>
+            entity?.updateSelfFrom($0)
          })
          self.saveIn(context: context, completion: completion)
       }
    }
-   override func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[DBRepoObjectModel]>) -> Void)) {
-      var objects: [DBRepoObjectModel] = []
-      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: associatedEntity)
+   override func save(_ rawData: [DBRepoRawData], completion: @escaping ((Result<Void>) -> Void)) {
+      contextSource.performBackgroundTask() { context in
+         // TO DO - Check case when CoreData not update someValues
+         guard let _ = NSEntityDescription.insertNewObject(forEntityName: self.associatedEntityName, into: context) as? EntityConvertable<DBRepoRawData, DBRepoExpectedModel> else { completion(Result(error: DBRepositoryErrors.entityConvertableMissing))
+            return assert(false, DBRepositoryErrors.entityConvertableMissing.localizedDescription)
+         }
+         
+         rawData.forEach({
+            let entity = NSEntityDescription.insertNewObject(forEntityName: self.associatedEntityName, into: context) as? EntityConvertable<DBRepoRawData, DBRepoExpectedModel>
+            entity?.updateSelfFrom($0)
+         })
+         self.saveIn(context: context, completion: completion)
+      }
+   }
+   override func get(by search: RepositorySearchRequest, completion: @escaping ((Result<[DBRepoExpectedModel]>) -> Void)) {
+      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: associatedEntityName)
       fetchRequest.predicate = search.predicate
       fetchRequest.sortDescriptors = search.sortDescriptors
       contextSource.performBackgroundTask() { context in
          do {
             let rawData = try context.fetch(fetchRequest)
-            guard let results = rawData as? [EntityConvertable] else { return }
-            
-            results.forEach({
-               guard let object = $0.transformSelfTo(expectedType: DBRepoObjectModel.self) else {return completion(Result(error: RepositoryErrors.entityConvertableMissing))}
-               objects.append(object)
-            })
-            return completion(Result(value: objects))
+            guard rawData.isEmpty == false else {return completion(Result(value: [])) }
+            guard let results = rawData as? [EntityConvertable<DBRepoRawData, DBRepoExpectedModel>] else {
+               completion(Result(value: []))
+               return assert(false, DBRepositoryErrors.entityConvertableMissing.localizedDescription)
+            }
+            let transformed = results.compactMap({ return $0.transformSelfToModel() })
+            return completion(Result(value: transformed))
          } catch {
             return completion(Result(error: error))
          }
       }
    }
    override func delete(by search: RepositorySearchRequest, completion: @escaping ((Result<Void>) -> Void)) {
-      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: associatedEntity)
+      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: associatedEntityName)
       fetchRequest.predicate = search.predicate
       contextSource.performBackgroundTask() { context in
          let results = try? context.fetch(fetchRequest)
@@ -166,5 +238,11 @@ final class DBRepository<DBRepoRawData: Codable, DBRepoObjectModel, DBRepoGettin
          self.saveIn(context: context, completion: completion)
       }
    }
+   
+   //MARK: - NSFetchedResultsControllerDelegate implementation
+   func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+      guard let fetchedObjects = controller.fetchedObjects as? [EntityConvertable<DBRepoRawData, DBRepoExpectedModel>] else { return }
+      let transformed = fetchedObjects.compactMap({ return $0.transformSelfToModel() })
+      searchedData?.value = transformed
+   }
 }
-
